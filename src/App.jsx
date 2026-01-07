f1ff46e0f44f09d4e00c0d93a71a2195751ef739
Ahmet Canbaz <ahmet-canbaz@hotmail.com>
2026-01-08 00:29:51 +0300

    install nanoid

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import ContactForm from "./components/ContactForm/ContactForm";
+
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Phonebook</h1>
+      <ContactForm />
     </>
   );
 }
