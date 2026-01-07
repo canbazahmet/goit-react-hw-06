44186bc357676e6fe24501a8ae0f54ceb1e76d82
Ahmet Canbaz <ahmet-canbaz@hotmail.com>
2026-01-08 00:08:06 +0300

    main.jsx edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import ContactForm from "./components/ContactForm/ContactForm.jsx";
-import SearchBox from "./components/SearchBox/SearchBox.jsx";
-
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
     </>
   );
 }
